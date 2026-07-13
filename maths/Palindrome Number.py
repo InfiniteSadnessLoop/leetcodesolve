2b946fee3d1e5d688dae662ad3e93fc2a6feb5be
tarekhossain90267-del <tarek.hossain90267@gmail.com>
2026-07-13 10:21:24 +0600

    Create Palindrome Number.py

---
+++ maths/Palindrome Number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+        if x % 10 == 0:
+            return False
+        orgx = x
+        numrev = 0
+        while x > 0:
+            lastdig = x % 10
+            numrev = numrev * 10 + lastdig
+            x = x//10 
+        return numrev == orgx                
